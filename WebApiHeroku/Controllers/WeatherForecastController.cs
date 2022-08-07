133df31c8c7631456b279bebbee244e19d468cd6
pawangithubact <pawan.edu930@gmail.com>
2022-08-07 12:43:32 +0530

    test push

--- WebApiHeroku/Controllers/WeatherForecastController.cs
+++ WebApiHeroku/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WebApiHeroku.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //test push 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
